665e158df5728c98404d625b752632af1f829f3f
vladhuk <vladhookovskiy@gmail.com>
2019-11-01 09:07:12 +0200

    Initial commit

---
+++ src/test/java/com/vladhuk/dept/api/DeptApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladhuk.dept.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeptApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

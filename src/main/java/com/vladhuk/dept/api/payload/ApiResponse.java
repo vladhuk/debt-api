f079ebf1b98ae53c916295fb6c17ad7d507598c1
vladhuk <vladhookovskiy@gmail.com>
2019-11-02 18:34:46 +0200

    Add payloads

---
+++ src/main/java/com/vladhuk/dept/api/payload/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.vladhuk.dept.api.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse {
+
+    private Boolean success;
+    private String message;
+
+}

67ec9d79438ff5e42d503096d860714ea374cbe2
vladhuk <vladhookovskiy@gmail.com>
2019-11-02 21:12:44 +0200

    Add service;

---
+++ src/main/java/com/vladhuk/dept/api/service/AuthenticationService.java
@@ -0,0 +1,13 @@
+package com.vladhuk.dept.api.service;
+
+import com.vladhuk.dept.api.model.User;
+
+public interface AuthenticationService {
+
+    String authenticateAndGetToken(String username, String password);
+
+    User registerUser(User user);
+
+    Boolean isUsernameExist(String username);
+
+}
---
+++ src/main/java/com/vladhuk/dept/api/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,66 @@
+package com.vladhuk.dept.api.service.impl;
+
+import com.vladhuk.dept.api.exception.AppException;
+import com.vladhuk.dept.api.model.Role;
+import com.vladhuk.dept.api.model.User;
+import com.vladhuk.dept.api.repository.RoleRepository;
+import com.vladhuk.dept.api.repository.UserRepository;
+import com.vladhuk.dept.api.security.JwtTokenProvider;
+import com.vladhuk.dept.api.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+
+@Service
+@Transactional
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private AuthenticationManager authenticationManager;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    public AuthenticationServiceImpl(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, JwtTokenProvider tokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public String authenticateAndGetToken(String username, String password) {
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(username, password)
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return tokenProvider.generateToken(authentication);
+    }
+
+    @Override
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        final Role userRole = roleRepository.findByName(Role.RoleName.USER)
+                .orElseThrow(() -> new AppException("User Role not set."));
+
+        user.setRoles(Collections.singleton(userRole));
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Boolean isUsernameExist(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+}

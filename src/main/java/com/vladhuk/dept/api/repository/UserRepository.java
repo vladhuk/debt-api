74adb58ae938f7d95386744f8bca5d728fb7cdf3
vladhuk <vladhookovskiy@gmail.com>
2019-11-02 17:45:26 +0200

    Improve repository;

--- src/main/java/com/vladhuk/dept/api/repository/UserRepository.java
+++ src/main/java/com/vladhuk/dept/api/repository/UserRepository.java
@@ -3,5 +3,12 @@ package com.vladhuk.dept.api.repository;
 import com.vladhuk.dept.api.model.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+    Optional<User> findById(Long id);
+
+    Optional<User> findByUsername(String username);
+
 }

e0e303c9c6be17fc1843162d199177b186689b60
vladhuk <vladhookovskiy@gmail.com>
2019-11-02 17:46:25 +0200

    Add security;

---
+++ src/main/java/com/vladhuk/dept/api/security/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.vladhuk.dept.api.security;
+
+import com.vladhuk.dept.api.model.User;
+import com.vladhuk.dept.api.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        final User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return UserPrincipal.create(user);
+    }
+
+    // This method is used by JWTAuthenticationFilter
+    public UserDetails loadUserById(Long id) {
+        final User user = userRepository.findById(id).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with id: " + id)
+        );
+
+        return UserPrincipal.create(user);
+    }
+}

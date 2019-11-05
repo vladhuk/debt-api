4353e1fbfcd3775eda57491932dedbcbf1a1659b
vladhuk <vladhookovskiy@gmail.com>
2019-11-06 01:52:16 +0200

    Add queries

--- src/main/java/com/vladhuk/dept/api/repository/GroupRepository.java
+++ src/main/java/com/vladhuk/dept/api/repository/GroupRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GroupRepository extends PagingAndSortingRepository<Group, Long> {
 
+    List<Group> findByOwnerId(Long id);
+
     List<Group> findByOwnerId(Long id, Pageable pageable);
+
+    Optional<Group> findByIdAndOwnerId(Long id, Long ownerId);
 
 }

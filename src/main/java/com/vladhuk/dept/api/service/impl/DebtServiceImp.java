a0166d74fdb1fb57cce3d7757fe8a11faa09bf0a
vladhuk <vladhookovskiy@gmail.com>
2019-11-06 09:43:58 +0200

    Implement methods

--- src/main/java/com/vladhuk/dept/api/service/impl/DebtServiceImp.java
+++ src/main/java/com/vladhuk/dept/api/service/impl/DebtServiceImp.java
@@ -1,7 +1,11 @@
 package com.vladhuk.dept.api.service.impl;
 
 import com.vladhuk.dept.api.model.Debt;
+import com.vladhuk.dept.api.repository.DebtRepository;
+import com.vladhuk.dept.api.service.AuthenticationService;
 import com.vladhuk.dept.api.service.DebtService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,13 +15,24 @@ import java.util.List;
 @Transactional
 public class DebtServiceImp implements DebtService {
 
+    private final DebtRepository debtRepository;
+    private final AuthenticationService authenticationService;
+
+    public DebtServiceImp(DebtRepository debtRepository, AuthenticationService authenticationService) {
+        this.debtRepository = debtRepository;
+        this.authenticationService = authenticationService;
+    }
+
     @Override
     public List<Debt> getAllDebts() {
-        return null;
+        final Long currentUserId = authenticationService.getCurrentUser().getId();
+        return debtRepository.findAllByCreditorIdOrBorrowerId(currentUserId, currentUserId);
     }
 
     @Override
     public List<Debt> getDebtsPage(Integer pageNumber, Integer pageSize) {
-        return null;
+        final Long currentUserId = authenticationService.getCurrentUser().getId();
+        final Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        return debtRepository.findAllByCreditorIdOrBorrowerId(currentUserId, currentUserId, pageable);
     }
 }

b870522c3cf875f42bb314d585c00bb2b71e091f
vladhuk <vladhookovskiy@gmail.com>
2019-11-06 10:40:06 +0200

    Add friends repo

---
+++ src/main/java/com/vladhuk/debt/api/repository/FriendRepository.java
@@ -0,0 +1,15 @@
+package com.vladhuk.debt.api.repository;
+
+import com.vladhuk.debt.api.model.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+public interface FriendRepository extends PagingAndSortingRepository<User, Long> {
+
+    @Query("SELECT friends FROM User WHERE id = ?1")
+    List<User> findAllFriendsByUserId(Long userId, Pageable pageable);
+
+}

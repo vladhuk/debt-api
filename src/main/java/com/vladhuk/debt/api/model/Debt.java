9d1977b7739a1f687fdd6bffec7adfafb59e9441
vladhuk <vladhookovskiy@gmail.com>
2019-11-06 17:55:32 +0200

    Add doc

--- src/main/java/com/vladhuk/debt/api/model/Debt.java
+++ src/main/java/com/vladhuk/debt/api/model/Debt.java
@@ -5,6 +5,16 @@ import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
+
+/**
+ * Uses for store the current balance between two users. Two users can have only one debt.
+ * If balance is positive, borrower owes money to creditor and if balance is negative,
+ * creditor owes money to borrower.
+ * Debt creates only with {@link DebtRequest}.
+ * The balance of debt can be changed only with {@link DebtRequest} or {@link RepaymentRequest}
+ *
+ * @author vladhuk
+ */
 @Entity
 @Table(name = "debts")
 @Data

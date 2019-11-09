f046ad89074436e7808464d196ac526e91af2170
vladhuk <vladhookovskiy@gmail.com>
2019-11-09 23:07:59 +0200

    Delete repository

--- src/main/java/com/vladhuk/debt/api/repository/RequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vladhuk.debt.api.repository;
-
-import com.vladhuk.debt.api.model.Request;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RequestRepository extends CrudRepository<Request, Long> {
-}

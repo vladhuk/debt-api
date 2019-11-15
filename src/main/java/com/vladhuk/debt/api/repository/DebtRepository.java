f870ce7296eb35ac5fae27c0a04dc9dac7bf8c16
vladhuk <vladhookovskiy@gmail.com>
2019-11-15 17:26:55 +0200

    Add query

--- src/main/java/com/vladhuk/debt/api/repository/DebtRepository.java
+++ src/main/java/com/vladhuk/debt/api/repository/DebtRepository.java
@@ -2,9 +2,12 @@ package com.vladhuk.debt.api.repository;
 
 import com.vladhuk.debt.api.model.Debt;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DebtRepository extends PagingAndSortingRepository<Debt, Long> {
 
@@ -12,6 +15,12 @@ public interface DebtRepository extends PagingAndSortingRepository<Debt, Long> {
 
     List<Debt> findAllByCreditorIdOrBorrowerId(Long creditorId, Long borrowerId, Pageable pageable);
 
+    Optional<Debt> findByCreditorIdOrBorrowerId(Long creditorId, Long borrowerId);
+
     Boolean existsByCreditorIdAndBorrowerId(Long creditorId, Long borrowerId);
+
+    @Modifying
+    @Query("DELETE FROM Debt WHERE id = ?1 and (creditor.id = ?2 or borrower.id = ?2)")
+    void deleteById_And_CreditorOrBorrowerId(Long id, Long creditorOrBorrowerId);
 
 }

c5cdf6ee4dc0040ed4e2c677a01c0969544998c7
vladhuk <vladhookovskiy@gmail.com>
2019-11-15 17:28:28 +0200

    - Add logging; - Refactor;

--- src/main/java/com/vladhuk/debt/api/service/impl/DebtServiceImp.java
+++ src/main/java/com/vladhuk/debt/api/service/impl/DebtServiceImp.java
@@ -1,19 +1,25 @@
 package com.vladhuk.debt.api.service.impl;
 
+import com.vladhuk.debt.api.exception.ResourceNotFoundException;
 import com.vladhuk.debt.api.model.Debt;
 import com.vladhuk.debt.api.repository.DebtRepository;
 import com.vladhuk.debt.api.service.AuthenticationService;
 import com.vladhuk.debt.api.service.DebtService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class DebtServiceImp implements DebtService {
+
+    private static final Logger logger = LoggerFactory.getLogger(DebtServiceImp.class);
 
     private final DebtRepository debtRepository;
     private final AuthenticationService authenticationService;
@@ -26,6 +32,7 @@ public class DebtServiceImp implements DebtService {
     @Override
     public List<Debt> getAllDebts() {
         final Long currentUserId = authenticationService.getCurrentUser().getId();
+        logger.info("Fetching all debts related to user with id {}", currentUserId);
         return debtRepository.findAllByCreditorIdOrBorrowerId(currentUserId, currentUserId);
     }
 
@@ -33,13 +40,24 @@ public class DebtServiceImp implements DebtService {
     public List<Debt> getDebtsPage(Integer pageNumber, Integer pageSize) {
         final Long currentUserId = authenticationService.getCurrentUser().getId();
         final Pageable pageable = PageRequest.of(pageNumber, pageSize);
+
+        logger.info("Fetching debts page related to user with id {}", currentUserId);
+
         return debtRepository.findAllByCreditorIdOrBorrowerId(currentUserId, currentUserId, pageable);
     }
 
-    // TODO: Make optional
     @Override
     public Debt getDebt(Long id) {
-        return debtRepository.findById(id).orElse(null);
+        final Long currentUserId = authenticationService.getCurrentUser().getId();
+        logger.info("Fetching debt with id {} related to user with id {}", id, currentUserId);
+        final Optional<Debt> debt = debtRepository.findByCreditorIdOrBorrowerId(currentUserId, currentUserId);
+
+        if (debt.isEmpty()) {
+            logger.error("Debt with id {} related to user with id {} not founded", id, currentUserId);
+            throw new ResourceNotFoundException("Debt", "id", id);
+        }
+
+        return debt.get();
     }
 
     @Override
@@ -52,16 +70,21 @@ public class DebtServiceImp implements DebtService {
 
     @Override
     public Debt createDebt(Debt debt) {
+        logger.info("Creating debt: {}", debt);
         return debtRepository.save(debt);
     }
 
     @Override
     public void deleteDebt(Long id) {
-        debtRepository.deleteById(id);
+        final Long currentUserId = authenticationService.getCurrentUser().getId();
+        logger.info("Deleting debt with id {} related to user with id {}", id, currentUserId);
+        debtRepository.deleteById_And_CreditorOrBorrowerId(id, currentUserId);
     }
 
     @Override
     public Debt addToBalance(Long debtId, Float cash) {
+        logger.info("Adding {} to balance of debt with id {}", cash, debtId);
+
         final Debt debt = getDebt(debtId);
         debt.setBalance(debt.getBalance() + cash);
         return debtRepository.save(debt);

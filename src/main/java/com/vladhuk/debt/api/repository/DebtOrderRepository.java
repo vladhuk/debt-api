93e714ac845467d58f29db6be971787e1f9b12f6
vladhuk <vladhookovskiy@gmail.com>
2019-11-19 19:03:41 +0200

    - Rename repository; - Fix queries;

--- src/main/java/com/vladhuk/debt/api/repository/DebtOrderRepository.java
+++ src/main/java/com/vladhuk/debt/api/repository/DebtOrderRepository.java
@@ -6,12 +6,14 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface OrderRepository extends CrudRepository<Order, Long> {
+public interface DebtOrderRepository extends CrudRepository<Order, Long> {
 
     @Query("SELECT ord FROM DebtRequest dr INNER JOIN dr.orders ord " +
             "WHERE dr.id = ?1 AND ord.receiver.id = ?2 AND ord.status.id = ?3")
     Optional<Order> findByDebtRequestIdAndReceiverIdAndStatusId(Long debtRequestId, Long receiverId, Long statusId);
 
+    @Query("SELECT count(ord) FROM DebtRequest dr INNER JOIN dr.orders ord " +
+            "WHERE ord.receiver.id = ?1 AND ord.status.id = ?2")
     Long countAllByReceiverIdAndStatusId(Long receiverId, Long statusId);
 
 }
